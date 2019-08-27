5e573dc59765f64e2e730c3fa090053961e9f633
Igor Bakhtin <ibakhtin@icloud.com>
2019-08-27 18:51:19 +0300

    completed part 011 and 012

--- src/connectDatabase.js
+++ src/connectDatabase.js
@@ -1,16 +1,22 @@
-import config from "config";
-import mongoose from "mongoose";
+import config from 'config'
+import mongoose from 'mongoose'
 
-const mongoDbConnectionString = config.get("mongoDbConnectionString");
+const mongoDbConnectionString = config.get('mongoDbConnectionString')
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(mongoDbConnectionString, {useNewUrlParser: true});
-    console.log("MongoDB connected.")
+    await mongoose.connect(
+      mongoDbConnectionString, 
+      {
+        useNewUrlParser: true, 
+        useCreateIndex: true 
+      }
+    )
+    console.log('MongoDB connected.')
   } catch (error) {
     console.error(error.message)
     process.exit(1)
   }
-};
+}
 
 export default connectDatabase
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,5 +1,11 @@
 import express from 'express'
-import { check, validationResult } from 'express-validator/check'
+import { check, validationResult } from 'express-validator'
+import gravatar from 'gravatar'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import config from 'config'
+
+import User from '../../models/User'
 
 const router = express.Router()
 
@@ -9,16 +15,57 @@ const router = express.Router()
 router.post(
   '/',
   [
-    check('name', 'Name is required').not().isEmpty(),
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
     check('email', 'Plese enter a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
+      res.status(400).json({ errors: errors.array() })
+      return
     }
-    res.status(200)
+
+    let { name, email, password } = req.body
+
+    try {
+      let user = await User.findOne({ email })
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] })
+        return
+      }
+
+      const avatar = gravatar.url(email, {
+        s: 200,
+        r: 'pg',
+        d: 'mm'
+      })
+
+      const salt = await bcrypt.genSalt(10)
+
+      password = await bcrypt.hash(password, salt)
+
+      user = new User({ name, email, avatar, password })
+
+      await user.save()
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 15552000 }, (error, token) => {
+        if (error) throw error
+        res.json({ token })
+      })
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send('Server error')
+    }
   }
 )
 
