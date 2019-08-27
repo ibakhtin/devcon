7208038b719939b6364d2fe745198e4b26c2fa90
Igor Bakhtin <ibakhtin@icloud.com>
2019-08-27 16:07:16 +0300

    completed part 010

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ const app = express()
 // Connect Database
 connectDatabase()
 
+// Init middleware
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) => res.send('API is running.'))
 
 // Define routes
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,10 +1,25 @@
 import express from 'express'
+import { check, validationResult } from 'express-validator/check'
 
-const router = express.Router();
+const router = express.Router()
 
-// @route GET api/users
-// @desc Test route
+// @route POST api/users
+// @desc Register user
 // @access public
-router.get('/', (req, res) => res.send('users route'))
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Plese enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    res.status(200)
+  }
+)
 
 export default router
