a82132e65f58cb1163b68e574e364f94ac79d532
Igor Bakhtin <ibakhtin@icloud.com>
2019-08-30 16:53:50 +0300

    completed part 024

---
+++ src/models/Post.js
@@ -0,0 +1,56 @@
+import { model, Schema } from 'mongoose'
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: user
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: user
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }    
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }    
+})
+
+const Post = model('post', PostSchema)
+
+export default Post
